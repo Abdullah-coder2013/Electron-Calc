7c898bd45abe3ff4de5936a2e35911b80fc230ba
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2022-10-07 15:10:17 +0200

    update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+require("update-electron-app")();
 const path = require("path");
 const createWindow = () => {
   const win = new BrowserWindow({
