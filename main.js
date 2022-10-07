88736cfa4a31dfe7fcd16dded77fabfac67b5c4f
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2022-10-07 13:15:19 +0200

    update

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 348,
+    height: 610,
+    icon: "./favicon.ico",
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "src/renderer.js")
+    },
+  });
+
+  win.loadFile("./src/index.html");
+  win.setMenu(null)
+  win.resizable = false;
+  
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+app.once("ready", () => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
---
+++ src/renderer.js
@@ -0,0 +1,79 @@
+const body = document.getElementById("body");
+let question = "";
+const result = document.getElementById("result");
+const operatorButtonIds = [
+  "btn-plus",
+  "btn-minus",
+  "btn-times",
+  "btn-divide",
+  "btn-mod",
+  "btn-bracket-open",
+  "btn-bracket-close",
+];
+let lastOperation = null;
+const operator_map = {
+  "+": "+",
+  "-": "-",
+  x: "*",
+  "÷": "/",
+  "%": "%",
+  "(": "(",
+  ")": ")",
+};
+
+for (let i = 0; i < 10; i++) {
+  document.getElementById(`btn-${i}`).addEventListener("click", function (e) {
+    if (lastOperation == "=") {
+      question = "";
+      result.value = "";
+    }
+    question = question + e.target.innerHTML;
+    result.value = result.value + e.target.innerHTML;
+    lastOperation = e.target.innerHTML;
+  });
+}
+for (let i = 0; i < 7; i++) {
+  document
+    .getElementById(operatorButtonIds[i])
+    .addEventListener("click", function (e) {
+      question = question + operator_map[e.target.innerHTML];
+      result.value = result.value + e.target.innerHTML;
+      lastOperation = operator_map[e.target.innerHTML];
+    });
+}
+document.getElementById("c").addEventListener("click", function onClick() {
+  result.value = "";
+  question = "";
+});
+document.getElementById("e").addEventListener("click", function onClick() {
+  var res = eval(question);
+  console.log(res);
+  result.value = res;
+  question = res;
+  lastOperation = "=";
+});
+
+document.getElementById("b").addEventListener("click", function onClick() {
+  question = question.substring(0, question.length - 1);
+  result.value = question;
+});
+
+document.getElementById("dot").addEventListener("click", function onClick(e) {
+  question = question + e.target.innerHTML;
+  result.value = result.value + e.target.innerHTML;
+  lastOperation = e.target.innerHTML;
+});
+
+document.getElementById("result").addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    run();
+  }
+});
+
+function run() {
+  let resultt = eval(result.value);
+  console.log(resultt);
+  result.value = resultt;
+  lastOperation = "=";
+  question = resultt;
+}
